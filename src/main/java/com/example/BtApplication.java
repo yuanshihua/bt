a0aaa9e1418692e0c596885b049fe67f52b982cf
yuanshihua <946896963@qq.com>
2017-11-24 16:57:39 +0800

    新增练习--斐波那契数列

--- src/main/java/com/example/BtApplication.java
+++ src/main/java/com/example/BtApplication.java
@@ -1,13 +1,20 @@
-package com.example.demo;
+package com.example;
 
+import java.util.stream.Stream;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import com.example.service.MathService;
 
 @SpringBootApplication
 public class BtApplication {
 
+	@Autowired
+	private MathService mathService;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(BtApplication.class, args);
 	}
@@ -15,6 +22,8 @@ public class BtApplication {
 	@Bean
 	public CommandLineRunner runner() {
 		return args -> {
+			Stream<Long> fs = mathService.factorialStream();
+			fs.limit(10).forEach(System.out::println);
 			System.out.println("Hello, Spring Boot!");
 		};
 	}
